82f2fd221a1e4e9463a9886d692864f6013a14b2
Mikhail <106593480+minyelling@users.noreply.github.com>
2022-05-31 15:37:08 +0300

    First commit
    First commit

---
+++ js/accordion.js
@@ -0,0 +1,8 @@
+(() => {
+$(".js-accordion").accordion({
+  collapsible: true,
+  active: false,
+  icons: false,
+  heightStyle: 'content'
+});
+})();
---
+++ js/index.js
@@ -0,0 +1,166 @@
+window.addEventListener('DOMContentLoaded',function() {
+// document.querySelector('#humburger').addEventListener('click',function() {
+// document.querySelector('#menu').classList.toggle('is-active')
+// })
+// document.querySelector('#menu').addEventListener('click',function() {
+//   document.querySelector('#menu').classList.toggle('is-active')
+// })
+// });
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+
+// ТАБЫ - ПЛАГИН
+const firstParams = {
+  tabsClass: "js-tab",
+  wrap: "js-tabs-wrap",
+  content: "js-tab-content",
+  active: "active"
+};
+
+const secondParams = {
+  tabsClass: "js-test-tab",
+  wrap: "js-tab-test-wrap",
+  content: "js-test-content",
+  active: "active"
+};
+
+
+function setTabs(params) {
+  const tabBtns = document.querySelectorAll(`.${params.tabsClass}`);
+
+  function onTabClick(e) {
+    const path = e.target.dataset.path;
+    const wrap = e.target.closest(`.${params.wrap}`);
+    const currentContent = wrap.querySelector(`.${params.content}[data-target="${path}"]`);
+    const contents = wrap.querySelectorAll(`.${params.content}`);
+
+    contents.forEach((el) => {
+      el.classList.remove(params.active);
+    });
+
+    currentContent.classList.add(params.active);
+
+    tabBtns.forEach((el) => {
+      el.classList.remove(params.active);
+    });
+
+    this.classList.add(params.active);
+  }
+
+  tabBtns.forEach(function (el) {
+    el.addEventListener("click", onTabClick);
+  });
+}
+
+setTabs(firstParams);
+
+
+// здесь мы определяем функцию, которая отвеает за работу меню, в ней не нужно ничего менять
+function setBurger(params) {
+  const btn = document.querySelector(`.${params.btnClass}`);
+  const menu = document.querySelector(`.${params.menuClass}`);
+
+  menu.addEventListener("animationend", function () {
+    if (this.classList.contains(params.hiddenClass)) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+    }
+  });
+
+  btn.addEventListener("click", function () {
+    this.classList.toggle(params.activeClass);
+
+    if (
+      !menu.classList.contains(params.activeClass) &&
+      !menu.classList.contains(params.hiddenClass)
+    ) {
+      menu.classList.add(params.activeClass);
+      document.body.style.overflow = 'hidden';
+    } else {
+      menu.classList.add(params.hiddenClass);
+      document.body.removeAttribute('style');
+    }
+  });
+}
+
+// здесь мы определяем функцию, которая отвеает за работу меню, в ней не нужно ничего менять
+function setBurger(params) {
+  const btn = document.querySelector(`.${params.btnClass}`);
+  const menu = document.querySelector(`.${params.menuClass}`);
+
+  menu.addEventListener("animationend", function () {
+    if (this.classList.contains(params.hiddenClass)) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+    }
+  });
+
+  btn.addEventListener("click", function () {
+    this.classList.toggle(params.activeClass);
+
+    if (
+      !menu.classList.contains(params.activeClass) &&
+      !menu.classList.contains(params.hiddenClass)
+    ) {
+      menu.classList.add(params.activeClass);
+      document.body.style.overflow = 'hidden';
+    } else {
+      menu.classList.add(params.hiddenClass);
+      document.body.removeAttribute('style');
+    }
+  });
+}
+
+// здесь мы вызываем функцию и передаем в нее классы наших элементов
+setBurger({
+  btnClass: "burger", // класс бургера
+  menuClass: "js-nav", // класс меню
+  activeClass: "is-opened", // класс открытого состояния
+  hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+});
+
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+console.log(search)
+  search.addEventListener("animationend", function () {
+    if (this.classList.contains(params.hiddenClass)) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+    }
+  });
+
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
+
+    if (
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
+    ) {
+      search.classList.add(params.activeClass);
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+}
+
+setSearch({
+  openBtnClass: "js-open-search", // класс кнопки открытия
+  closeBtnClass: "js-close-search", // класс кнопки закрытия
+  searchClass: "js-form", // класс формы поиска
+  activeClass: "is-opened", // класс открытого состояния
+  hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+});
+});
